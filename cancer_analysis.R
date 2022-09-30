88782d70b6f35eebe68430a0d01263eacf56823f
Tapur Grewal <114151150+TapurGrewal@users.noreply.github.com>
2022-09-30 21:50:37 +0530

    Update cancer_analysis.R

--- cancer_analysis.R
+++ cancer_analysis.R
@@ -14,18 +14,5 @@ func = function(x){
 data = func(x)
 data
 
-zscore =function(data){
-  for (i in 1:ncol(data)){
-    z_score = (data- rowMeans(data))/rowSds(as.matrix(data))[row(data)]
-    
-  }
-  z_score[is.na(z_score)]=0
-  zcs = as.matrix(z_score)
-  return(zcs)
-
-
-}
-analysis = zscore(data)
-analysis
 
 
